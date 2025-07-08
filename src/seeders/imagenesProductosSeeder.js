5c1865e9adb4949aa6a950f1fb4bf085468d0baa
edward200167 <eyllanes@sinapsiscode.com>
2025-07-08 15:21:17 -0500

    actualizaciones 6

--- src/seeders/imagenesProductosSeeder.js
+++ src/seeders/imagenesProductosSeeder.js
@@ -7,64 +7,54 @@ const imagenesProductosSeeder = async () => {
         id: 1,
         url: 'https://promart.vteximg.com.br/arquivos/ids/8550084-444-444/30102.jpg?v=638727513841270000',
         descripcion: 'Primera imagen de Placa de Yeso Estándar 1/2',
-        productoId: 1
       },
       {
         id: 2,
         url: 'https://promart.vteximg.com.br/arquivos/ids/8550085-1000-1000/30102_0.jpg?v=638727513846600000',
         descripcion: 'Segunda imagen de Placa de Yeso Estándar 1/2',
-        productoId: 1
       },
       {
         id: 3,
         url: 'https://promart.vteximg.com.br/arquivos/ids/8550086-1000-1000/30102_1.jpg?v=638727513848000000',
         descripcion: 'Tercera imagen de Placa de Yeso Estándar 1/2',
-        productoId: 1
       },
       //Canaleta con adhesivo 20x12mm Schneider Electric
       {
         id: 4,
         url: 'https://promart.vteximg.com.br/arquivos/ids/7350697-1000-1000/49964.jpg?v=638255829782430000',
         descripcion: 'Primera imagen Canaleta con adhesivo 20x12mm Schneider Electrico',
-        productoId: 2
       },
       {
         id: 5,
         url: 'https://promart.vteximg.com.br/arquivos/ids/7350698-1000-1000/49964_0.jpg?v=638255829786270000',
         descripcion: 'Segunda imagen Canaleta con adhesivo 20x12mm Schneider Electrico',
-        productoId: 2
       },
       {
         id: 6,
         url: 'https://promart.vteximg.com.br/arquivos/ids/7350699-1000-1000/49964_4.jpg?v=638255829790800000',
         descripcion: 'Tercera imagen Canaleta con adhesivo 20x12mm Schneider Electrico',
-        productoId: 2
       },
       //Placa Yeso St 1/2\" 12.7mm 1.22x2.44m Volcanita
       {
         id: 7,
         url: 'https://promart.vteximg.com.br/arquivos/ids/8549593-1000-1000/14522.jpg?v=638727489696970000',
         descripcion: 'Primera imagen Placa Yeso St 1/2\" 12.7mm 1.22x2.44m Volcanita',
-        productoId: 3
       },
       {
         id: 8,
         url: 'https://promart.vteximg.com.br/arquivos/ids/8549594-1000-1000/14522_0.jpg?v=638727489701330000',
         descripcion: 'Segunda imagen Placa Yeso St 1/2\" 12.7mm 1.22x2.44m Volcanita',
-        productoId: 3
       },
       {
         id: 9,
         url: 'https://promart.vteximg.com.br/arquivos/ids/8549595-1000-1000/14522_1.jpg?v=638727489702770000',
         descripcion: 'Tercera imagen Placa Yeso St 1/2\" 12.7mm 1.22x2.44m Volcanita',
-        productoId: 3
       },
       //Estufa Industrial 3000W Orange
       {
         id: 10,
         url: 'https://promart.vteximg.com.br/arquivos/ids/8721531-1000-1000/161511.jpg?v=638784537037330000',
         descripcion: 'Primera imagen Estufa Industrial 3000W Orange',
-        productoId: 4
       },
     ];
 
